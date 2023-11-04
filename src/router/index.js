ad34df19c0e1af4642fbfd94a0fce4f02593e977
Huaji233 <1436615337@qq.com>
2023-11-05 01:36:00 +0800

    完成了路由的登陆检验

--- src/router/index.js
+++ src/router/index.js
@@ -7,6 +7,8 @@ import ProblemPage from '../components/ProblemPage.vue'
 import UserPage from '../components/UserPage.vue'
 import ManagePage from '../components/ManagePage.vue'
 
+import { jwtDecode } from "jwt-decode";
+
 Vue.use(VueRouter)
 
 const routes = [
@@ -23,18 +25,21 @@ const routes = [
   {
     path: '/Problem',
     name: 'Problem',
-    component: ProblemPage
+    component: ProblemPage,
+    meta: { requiresAuth: true }
   },
   {
     path: '/User',
     name: 'User',
-    component: UserPage
+    component: UserPage,
+    meta: { requiresAuth: true }
   }
   ,
   {
     path: '/Manage',
     name: 'Manage',
-    component: ManagePage
+    component: ManagePage,
+    meta: { requiresAuth: true, requiresAdmin: true }
   }
 ]
 
@@ -48,5 +53,46 @@ const originalPush = VueRouter.prototype.push
 VueRouter.prototype.push = function push(location) {
   return originalPush.call(this, location).catch(err => err)
 }
+
+
+router.beforeEach((to, from, next) => {
+  let token = localStorage.getItem("token");
+
+  if (to.meta.requiresAuth) {
+    let decoded
+    try {
+      decoded = jwtDecode(token);
+    } catch (error) {
+      next({
+        path: '/Login'
+      })
+      return
+    }
+
+    let isLogin = true
+    console.log(decoded);
+    let roleID = decoded.roleID
+    console.log(isLogin, to.meta.requiresAuth, to.meta.requiresAdmin);
+    if (isLogin && !to.meta.requiresAdmin) {
+      next()
+    }
+    else if (isLogin && to.meta.requiresAdmin) {
+      if (roleID == 1) next()
+      else {
+        next({
+          path: '/'
+        })
+      }
+    }
+    else {
+      next({
+        path: '/Login'
+      })
+    }
+  } else {
+    next()
+  }
+})
+
 
 export default router
